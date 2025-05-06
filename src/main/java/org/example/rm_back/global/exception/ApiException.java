4d37932ddfc3ebcdba89ea7af73882b4fc05e60d
dodu232 <dodu7510@gmail.com>
2025-05-06 13:12:18 +0900

    feat: exception 추가

---
+++ src/main/java/org/example/rm_back/global/exception/ApiException.java
@@ -0,0 +1,13 @@
+package org.example.rm_back.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public class ApiException extends RuntimeException {
+    private final String message;
+    private final ErrorType errorType;
+    private final HttpStatus httpStatus;
+}
---
+++ src/main/java/org/example/rm_back/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package org.example.rm_back.global.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+        MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<String> handleApiException(ApiException e) {
+        log.error("ApiException occurred. type={} message={} className={}", e.getErrorType(),
+            e.getMessage(), e.getClass().getName());
+        return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+    }
+}
