da4a3c3de65a590687c838a77c6a07f8731d32f4
dodu232 <dodu7510@gmail.com>
2025-05-12 17:48:20 +0900

    refactor: valid 추가

--- src/main/java/org/example/rm_back/admin/dto/AdminRequestDto.java
+++ src/main/java/org/example/rm_back/admin/dto/AdminRequestDto.java
@@ -3,6 +3,7 @@ package org.example.rm_back.admin.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 public class AdminRequestDto {
 
@@ -11,11 +12,13 @@ public class AdminRequestDto {
     public static class Add{
 
         @NotBlank(message = "account 필수 입력")
+        @Length(min = 4, max = 30, message = "4~30자까지 입력 가능")
         private String account;
 
         private String[] role;
 
         @NotBlank(message = "password 필수 입력")
+        @Length(min = 4, max = 12, message = "4~12자까지 입력 가능")
         private String password;
     }
 }
