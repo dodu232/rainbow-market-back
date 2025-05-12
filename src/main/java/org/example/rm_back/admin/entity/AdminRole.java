3853117c30708fad0c8dbe4d5d31b18c728a4629
dodu232 <dodu7510@gmail.com>
2025-05-12 17:00:27 +0900

    refactor: apiException 적용

--- src/main/java/org/example/rm_back/admin/entity/AdminRole.java
+++ src/main/java/org/example/rm_back/admin/entity/AdminRole.java
@@ -1,6 +1,9 @@
 package org.example.rm_back.admin.entity;
 
 import lombok.Getter;
+import org.example.rm_back.global.exception.ApiException;
+import org.example.rm_back.global.exception.ErrorType;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum AdminRole {
@@ -21,7 +24,7 @@ public enum AdminRole {
             try{
                 result.append(AdminRole.valueOf(s.toUpperCase())).append(", ");
             } catch (IllegalArgumentException e){
-                throw new RuntimeException();
+                throw new ApiException("올바르지 않은 USER ROLE", ErrorType.INVALID_PARAMETER, HttpStatus.BAD_REQUEST);
             }
         }
         return result.toString();
