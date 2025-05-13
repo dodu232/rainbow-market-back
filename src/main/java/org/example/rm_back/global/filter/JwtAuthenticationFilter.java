e7c0ec70b74ffd9214da9246a5f9c9e4653e6a37
dodu232 <dodu7510@gmail.com>
2025-05-13 19:41:48 +0900

    refactor: url에 따라 분기 처리 추가

--- src/main/java/org/example/rm_back/global/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/example/rm_back/global/filter/JwtAuthenticationFilter.java
@@ -10,30 +10,35 @@ import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.example.rm_back.auth.UserPrincipal;
 import org.example.rm_back.global.jwt.JwtProvider;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/auth/signin")
+            || HttpMethod.OPTIONS.matches(request.getMethod());
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res,
+        FilterChain chain)
         throws ServletException, IOException {
         String header = req.getHeader("Authorization");
-        if(header != null && header.startsWith("Bearer ")){
+
+        if (header != null && header.startsWith("Bearer ")) {
             String token = header.substring(7);
             try {
                 Claims claims = jwtProvider.validToken(token);
                 UserPrincipal principal = new UserPrincipal(
                     claims.getSubject(),
-                    claims.get("roles", String[].class)
+                    claims.get("roles", String.class)
                 );
                 req.setAttribute("currentUser", principal);
             } catch (JwtException e) {
