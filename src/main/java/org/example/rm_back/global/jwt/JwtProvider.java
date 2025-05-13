df3100031ab3efbdae193bed4055ae499e061b5e
dodu232 <dodu7510@gmail.com>
2025-05-13 19:50:28 +0900

    refactor: 토큰 유효성 검사 추가

--- src/main/java/org/example/rm_back/global/jwt/JwtProvider.java
+++ src/main/java/org/example/rm_back/global/jwt/JwtProvider.java
@@ -1,12 +1,19 @@
 package org.example.rm_back.global.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import lombok.extern.slf4j.Slf4j;
+import org.example.rm_back.global.exception.ApiException;
+import org.example.rm_back.global.exception.ErrorType;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,4 +44,22 @@ public class JwtProvider {
             .signWith(key)
             .compact();
     }
+
+    public Claims validToken(String token) {
+        try {
+            return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            throw new ApiException("잘못된 토큰", ErrorType.INVALID_TOKEN,
+                HttpStatus.UNAUTHORIZED); // 토큰이 잘못된 경우 예외 처리
+        } catch (ExpiredJwtException e) {
+            throw new ApiException("만료된 토큰", ErrorType.INVALID_TOKEN,
+                HttpStatus.UNAUTHORIZED); // 토큰이 만료된 경우 예외 처리
+        } catch (UnsupportedJwtException | IllegalArgumentException e) {
+            throw new ApiException("지원하지 않는 토큰", ErrorType.INVALID_TOKEN,
+                HttpStatus.UNAUTHORIZED); // 지원하지 않는 토큰이거나 잘못된 형식의 경우 예외 처리
+        } catch (Exception e) {
+            throw new ApiException("알 수 없는 에러", ErrorType.INVALID_TOKEN, HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
