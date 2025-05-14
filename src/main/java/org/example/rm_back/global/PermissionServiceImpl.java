f77d5866ecc354669240af4fa4608c041e745a97
dodu232 <dodu7510@gmail.com>
2025-05-14 16:51:53 +0900

    refactor: 코드 리뷰 반영 - http 메소드 별 권한 설정할 수 있도록 수정

--- src/main/java/org/example/rm_back/global/PermissionServiceImpl.java
+++ src/main/java/org/example/rm_back/global/PermissionServiceImpl.java
@@ -2,6 +2,8 @@ package org.example.rm_back.global;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.example.rm_back.admin.enums.AdminRole;
 import org.example.rm_back.auth.UserPrincipal;
@@ -11,29 +13,38 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PermissionServiceImpl implements PermissionService {
 
-    private final Map<String, String[]> permissionMap = Map.of(
-        "/api/v1/admin", new String[]{AdminRole.SUPER.name()}
+    private final Map<String, Map<String, Set<AdminRole>>> permissionMap = Map.of(
+        "/api/v1/admin", Map.of(
+            "POST", Set.of(AdminRole.SUPER)
+        )
     );
 
     @Override
     public boolean hasAccess(UserPrincipal user, String path, String method) {
-        if (!permissionMap.containsKey(path)) {
+        Map<String, Set<AdminRole>> methodMap = permissionMap.get(path);
+        if (methodMap == null) {
             return false;
         }
 
-        String[] requiredRoles = permissionMap.get(path);
-        if (requiredRoles.length == 0) {
+        Set<AdminRole> requiredRoles = methodMap.get(method);
+        if (requiredRoles == null) {
+            return false;
+        }
+
+        if (requiredRoles.isEmpty()) {
             return true;
         }
 
-        String[] userRoles = AdminRole.toRoleArray(user.getRoles());
+        Set<AdminRole> userRoles = Arrays.stream(AdminRole.toRoleArray(user.getRoles()))
+            .map(AdminRole::valueOf)
+            .collect(Collectors.toSet());
 
-        for (String s : requiredRoles) {
-            if (Arrays.stream(userRoles)
-                .anyMatch(role -> role.equalsIgnoreCase(s))) {
+        for (AdminRole r : userRoles) {
+            if (requiredRoles.contains(r)) {
                 return true;
             }
         }
+
         return false;
     }
 }
